2a45430592ad19f6915d3c47ba653170d01628f3
Ankit-Karn <ankitkarn10@gmail.com>
2023-05-29 16:04:20 +0530

    signin page

---
+++ src/components/pages/Signin.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom';
+
+const Signin = () => {
+
+    const navigate = useNavigate();
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    function handleEmail(e){
+        setEmail(e.target.value)
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        localStorage.setItem("Email", email);
+        localStorage.setItem("Password", password);
+        navigate('/home')
+    }
+
+    function handleForgotPassword(){
+        console.log('hello')
+    }
+
+    return(
+        <>
+            <div>
+                <h1>Sign in to Twitter</h1>
+                <button>Sign in with Google</button>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Email: 
+                        <input type="email" value={email} onChange={handleEmail}/>
+                    </label>
+                    <br />
+                    <label>
+                        Password:
+                        <input type="password" value={password} onChange={handlePassword} />
+                    </label>
+                    <br />
+                    <button type="submit">Log in</button>
+                    <br />
+                </form>
+                <button onClick={handleForgotPassword}>Forgot Password?</button>
+            </div> 
+        </>
+    )
+}
+
+export default Signin;
