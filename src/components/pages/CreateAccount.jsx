7e9f2baf5df9164d60597a22f095e256462930c0
Ankit-Karn <ankitkarn10@gmail.com>
2023-06-02 10:40:53 +0530

    All Twitter

--- src/components/pages/CreateAccount.jsx
+++ src/components/pages/CreateAccount.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from './CreateAccount.module.css';
 import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
+import { SiTwitter } from 'react-icons/si';
+
+
 
 
 
@@ -12,20 +17,31 @@ export default function CreateAccount() {
     navigate('/signup')
   }
 
-
+  function getBackToSignin(){
+    navigate('/signin')
+  }
 
   return (
-    <div className={styles.main}>
-      <div>
-        <img src='https://img.freepik.com/free-icon/twitter_318-674515.jpg' />
-        <h2>Join Twitter today</h2>
-        <button className={styles.googleButton}><img className={styles.googleLogo} src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png' /> <h4 className={styles.google}>Sign up with Google</h4></button>
+    <div>
+      <div className={styles.main}>
 
-        <button className={styles.appleButton}><img src='https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg' className={styles.appleLogo} /><h4 className={styles.google}>Sign up with Apple</h4></button>
-        <h4>or</h4>
-        <button className={styles.accountButton} onClick={handleClick}><h4>Create account</h4></button>
+      <div className={styles.img}>
+         <SiTwitter className={styles.Twticon} />
+      </div>
+       
+        <h2 className={styles.heading2}>Join Twitter today</h2>
+
+
+        <div className={styles.btn}>
+        <button className={styles.googleButton}><FcGoogle />Sign up with Google</button>
+
+           <button className={styles.appleButton}> <FaApple  />Sign up with Apple</button>
+           <h3 className={styles.heading-3}>or</h3>
+           <button className={styles.accountButton} onClick={handleClick}><h4>Create account</h4></button>
+        </div>
+    
         <p>By signing up, you agree to the <a>Terms of Service</a> and <a>Privacy Policy</a>, including <a>Cookie Use.</a></p>
-        <h4>Have an account already? <a>Log in</a></h4>
+        <h4 className={styles.heading4}>Have an account already? <a onClick={getBackToSignin}>Log in</a></h4>
       </div>
     </div>
   )
